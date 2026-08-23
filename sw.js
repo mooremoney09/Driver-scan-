3c3964ea838339ab3ea663698d22520574df60b5
mooremoney09 <chinajordan260@gmail.com>
2026-08-23 19:38:31 -0400

    Add files via upload
    Update next page job finder

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='driverscan-reliable-walmart-v4';
+const CACHE='driverscan-demo-offers-v5';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))));
