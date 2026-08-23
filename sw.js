fa011818fa97605833dce5cfd3a91b5d6e1e7506
mooremoney09 <chinajordan260@gmail.com>
2026-08-23 19:06:38 -0400

    Add files via upload
    New nearby location stable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='driverscan-nearby-walmart-v2';
+const CACHE='driverscan-stable-nearby-v3';
 self.addEventListener('install',e=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))));
