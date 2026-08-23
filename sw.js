776af41fb909276a3114cba5b4227f9edb9a8da7
mooremoney09 <chinajordan260@gmail.com>
2026-08-23 15:28:51 -0400

    Add files via upload
    Final update

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-const CACHE='driverscan-v5-two-barcodes';
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
-));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))));
+const CACHE='driverscan-final-v1';const ASSETS=['./','./index.html','./manifest.json','./assets/opening.png','./assets/spark-driver.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
