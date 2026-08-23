b54939b4e07f11b840ca35eae6b5e83aedaf6fba
mooremoney09 <chinajordan260@gmail.com>
2026-08-23 18:39:54 -0400

    Add files via upload
    Update on live GPS in job searching

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-const CACHE='driverscan-full-flow-summary-v2';
+const CACHE='driverscan-live-home-v1';
 self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(
-  caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
-));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request))));
